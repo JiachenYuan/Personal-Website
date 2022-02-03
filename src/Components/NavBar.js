8d22969e4635bee65fb2e38a5142adc7d376c1d8
Jiachen Yuan <67255516+JiachenYuan@users.noreply.github.com>
2022-02-03 10:54:32 -0800

    Updated Resume Link

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -36,7 +36,7 @@ function NavBar() {
         </Nav>
 
         <Nav className='mr-auto d-flex flex-wrap'>
-          <Nav.Link href="https://drive.google.com/file/d/151R2LEwp46HeZAC3ErsorBMAebfw3yUY/view?usp=sharing" target="_blank" className="hvr-underline-from-center">
+          <Nav.Link href="https://drive.google.com/file/d/1cujUHwgAfRf__wTdSOK-8HbNlbDkyCHx/view?usp=sharing" target="_blank" className="hvr-underline-from-center">
             <span style={{textDecoration:"underline", wordBreak:"break-word"}}>
               Resume
             </span>
